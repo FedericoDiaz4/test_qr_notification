b26b9e37faa58c46a2dcfbc9b35dde01a6344de0
Federico Diaz <federico.diaz4@gmail.com>
2024-02-22 14:39:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express, { response } from "express";
+
+const app = express();
+const url = "/api";
+
+app.use(express.json());
+
+app.get(`${url}/test`, (request, response) => {
+  console.log("FUNCIONA PA");
+  response.json({
+    code: "200",
+    status: "OK",
+  });
+});
+
+app.post(`${url}/test`, (request, response) => {
+  console.log("recibido");
+  response.json({
+    code: "200",
+    status: "OK",
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
